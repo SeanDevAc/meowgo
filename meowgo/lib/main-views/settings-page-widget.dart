57b308a79d2dfefc3089980c05d9c4b6227ab287
SeanDevAc <seanwakker.website@gmail.com>
2023-07-03 00:35:42 +0200

    Settings safety

--- meowgo/lib/main-views/settings-page-widget.dart
+++ meowgo/lib/main-views/settings-page-widget.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:meowgo/component/db_helper.dart';
 
-class SettingsStatsWidget extends StatelessWidget {
+class SettingsStatsWidget extends StatefulWidget {
   const SettingsStatsWidget({Key? key}) : super(key: key);
+
+  @override
+  _SettingsStatsWidgetState createState() => _SettingsStatsWidgetState();
+}
+
+class _SettingsStatsWidgetState extends State<SettingsStatsWidget> {
+  bool isUnlocked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +20,10 @@ class SettingsStatsWidget extends StatelessWidget {
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
           colors: [
-            Colors.red, 
-            Colors.red, 
-            Colors.white, 
-            Colors.white, 
+            Colors.red,
+            Colors.red,
+            Colors.white,
+            Colors.white,
           ],
           stops: [0.0, 0.5, 0.5, 1.0],
         ),
@@ -42,26 +49,45 @@ class SettingsStatsWidget extends StatelessWidget {
             width: 140,
             height: 140,
             decoration: BoxDecoration(
-              color: Colors.red.shade50, 
+              color: Colors.red.shade50,
               shape: BoxShape.circle,
             ),
           ),
           Column(
             children: [
-              SizedBox(height: 20.0), 
-              TextButton(
-                onPressed: () => _showResetConfirmationDialog(context),
-                child: Container(
-                  padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20.0),
-                    color: Colors.white,
-                  ),
-                  child: Text(
-                    "Reset Collection",
-                    style: TextStyle(
-                      color: Colors.red, 
-                      fontWeight: FontWeight.bold,
+              SizedBox(height: 20.0),
+              ElevatedButton.icon(
+                onPressed: () {
+                  setState(() {
+                    isUnlocked = !isUnlocked;
+                  });
+                },
+                icon: Icon(isUnlocked ? Icons.lock_open : Icons.lock),
+                label: Text(isUnlocked ? "Lock Reset Button" : "Unlock Reset Button"),
+              ),
+              SizedBox(height: 20.0),
+              Opacity(
+                opacity: isUnlocked ? 1.0 : 0.5,
+                child: TextButton(
+                  onPressed: isUnlocked ? () => _showResetConfirmationDialog(context) : null,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20.0),
+                      color: Colors.white,
+                    ),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        SizedBox(width: 8.0),
+                        Text(
+                          "Reset Collection",
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
@@ -79,7 +105,8 @@ class SettingsStatsWidget extends StatelessWidget {
       builder: (BuildContext context) {
         return AlertDialog(
           title: Text("Reset Collection"),
-          content: Text("Are you sure you want to reset your Pokemon collection?"),
+          content:
+              Text("Are you sure you want to reset your Pokemon collection?"),
           actions: [
             TextButton(
               onPressed: () {
